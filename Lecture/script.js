80058f8cf26d40333628e58356a14a1b1a23002f
Murshyne <murshyne@gmail.com>
2024-09-07 00:03:34 -0400

    Data manipulation using java first Commit

---
+++ Lecture/script.js
@@ -0,0 +1,16 @@
+const a = 5;
+const b = 10;
+
+console.log(
+  "'a' is assigned a value of \"" +
+    a +
+    ",\" and 'b' is assigned a value of \"" +
+    b +
+    '."\n' +
+    "Therefore, 'a' plus 'b' is equal to " +
+    (a + b) +
+    "."
+);
+// 'a' is assigned a value of "5," and 'b' is assigned a value of "10."
+// Therefore, 'a' plus 'b' is equal to 15.
+console.log("5"+10)
